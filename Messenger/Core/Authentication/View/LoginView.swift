5e2da70ada4cbc9d545bbedc72f795012f92a145
Sam Neat <samjneat@gmail.com>
2024-07-29 08:50:52 +0100

    initial app set up

---
+++ Messenger/Core/Authentication/View/LoginView.swift
@@ -0,0 +1,32 @@
+//
+//  LoginView.swift
+//  Messenger
+//
+//  Created by Sam Neat on 29/07/2024.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                //logo
+                
+                //text fields
+                
+                //forgot password
+                
+                //login button
+                
+                //facebook login
+                
+                //sign up link
+            }
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}
