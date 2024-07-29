958343b2e6990d680f80abec13029c0951599531
Sam Neat <samjneat@gmail.com>
2024-07-29 08:40:25 +0100

    Initial Commit

---
+++ Messenger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Messenger
+//
+//  Created by Sam Neat on 29/07/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Messenger/MessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerApp.swift
+//  Messenger
+//
+//  Created by Sam Neat on 29/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct MessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
