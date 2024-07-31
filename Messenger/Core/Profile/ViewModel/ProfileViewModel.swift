e00025c4e3c46c32a817c3cd9d2c5571b3780f70
Sam Neat <samjneat@gmail.com>
2024-07-31 12:02:25 +0100

    create image picker so user can select profile image

---
+++ Messenger/Core/Profile/ViewModel/ProfileViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileViewModel.swift
+//  Messenger
+//
+//  Created by Sam Neat on 31/07/2024.
+//
+
+import Foundation
+import SwiftUI
+import PhotosUI
+
+class ProfileViewModel: ObservableObject {
+    @Published var selectedItem: PhotosPickerItem? {
+        didSet { Task { try await loadImage() } }
+    }
+    
+    @Published var profileImage: Image?
+    
+    func loadImage() async throws {
+        guard let item = selectedItem else { return }
+        guard let imageData = try await item.loadTransferable(type: Data.self) else { return }
+        guard let uiImage = UIImage(data: imageData) else { return }
+        self.profileImage = Image(uiImage: uiImage)
+    }
+}
